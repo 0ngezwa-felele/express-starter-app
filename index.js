bbe8da3958a4cd4727dd1f0550793bcadb1b6fd9
0ngezwa-felele <ongezwa@codex.com>
2021-09-13 18:38:31 +0200

    all the files changes

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const exphbs  = require('express-handlebars');
+let small 
+let medium 
+let large 
+let grandTotal
+let size 
 
 const app = express();
-const PORT =  process.env.PORT || 3017;
+// const PORT =  process.env.PORT || 3017;
 
 // enable the req.body object - to allow us to use HTML forms
 app.use(express.json());
@@ -16,21 +21,45 @@ app.use(express.static('public'));
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
-let counter = 0;
 
 app.get('/', function(req, res) {
+
 	res.render('index', {
-		counter
+	
 	});
 });
 
-app.post('/count', function(req, res) {
-	counter++;
-	res.redirect('/')
-});
+app.post('/small', function (req, res) {
+	if(size == 'small'){
+		small == 33
+	}
+
+	res.redirect('/');
+
+})
+ 
+app.post('/medium', function (req, res) {
+
+	res.redirect('/');
+
+})
+
+app.post('/large', function (req, res) {
+
+	res.redirect('/');
+
+})
+
+app.post('/count', function (req, res) {
+
+	res.redirect('/');
+
+})
+
 
 
 // start  the server and start listening for HTTP request on the PORT number specified...
-app.listen(PORT, function() {
-	console.log(`App started on port ${PORT}`)
-});
+const PORT = process.env.PORT || 7002;
+app.listen(PORT, function () {
+	console.log('started');
+})
